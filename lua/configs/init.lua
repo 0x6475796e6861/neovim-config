7b1a7e78258bb4e6b5416289a9139570db2bafed
Nguyễn Hoàng Anh Duy <me@duynha.dev>
2025-06-30 15:13:47 +0700

    Update init.lua with enhanced editor settings
    Add clipboard integration, improved indentation settings, and smart formatting options.

--- lua/configs/init.lua
+++ lua/configs/init.lua
@@ -1,2 +1,14 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
+local set = vim.opt
+
+set.clipboard = "unnamedplus"
+
+set.number = true
+set.relativenumber = true
+
+set.tabstop     = 4
+set.softtabstop = 4
+set.shiftwidth  = 4
+set.expandtab   = true
+set.shiftround  = true
+set.smartindent = true
+set.cindent     = true
